febe3ef98fe72b577bb85fdb20e3502f54119b5b
Bryan Vera <brayanandresveragar@gmail.com>
2023-07-20 17:20:47 -0500

    Se realiza la construccion del metodo get

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,8 @@ function App() {
   const [cargo,setCargo] = useState("");
   const [antiguedad,setAntiguedad] = useState(0);
 
+  const [empleadosList, setEmpleados] = useState([]);
+
   const add = () => {
     axios.post('http://localhost:3001/create', {
       nombre: nombre,
@@ -18,12 +20,19 @@ function App() {
       cargo: cargo,
       antiguedad: antiguedad
     }).then(() => {
-      alert.log("Success");
+      console.log("Success");
+    });
+  }
+
+  const getEmpleados = () => {
+    axios.get('http://localhost:3001/empleados').then((response) => {
+      console.log(empleadosList);
     });
   }
 
   return (
     <div className="App">
+
       <div className="datos">
         <label>Nombre <input 
         onChange = {(event) => {
@@ -56,7 +65,17 @@ function App() {
         type="number"></input></label>
 
         <button className="btn-registrar" onClick={add}>Registrar</button>  
-      </div>  
+      </div>
+
+      <div className="lista">
+        <button className="" onClick={getEmpleados}>Consultar</button>
+
+        {
+          empleadosList.map((empleado) => {
+            return <div key={empleado.ID} className=""> {empleado.NAME}</div>
+          })
+        }
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -6,14 +6,16 @@ const cors  = require('cors');
 app.use(cors());
 app.use(express.json());
 
+//  INFO Configuracion de la base de datos
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'username',
-    password: 'password',
-    port: 3307,
+    user: 'root',
+    password: 'root',
+    port: 33061,
     database: 'personas'
 })
 
+//  INFO Función de insercción a la base de datos con el metodo post
 app.post('/create', (req, res) => {
     const nombre = req.body.nombre;
     const edad = req.body.edad;
@@ -21,7 +23,7 @@ app.post('/create', (req, res) => {
     const cargo = req.body.cargo;
     const antiguedad = req.body.antiguedad;
     
-    db.query('INSERT INTO empleados (nombre, edad, pais, cargo, antiguedad) VALUES (?,?,?,?,?)',[nombre,edad,pais,cargo,antiguedad],
+    db.query('INSERT INTO empleados (name, edad, pais, cargo, antiguedad) VALUES (?,?,?,?,?)',[nombre,edad,pais,cargo,antiguedad],
     (err,result)=>{
         if(err){
             console.log(err)
@@ -30,5 +32,20 @@ app.post('/create', (req, res) => {
         }
     }) //Promesa de esos valores
 });
+
+//  INFO Función de lectura de la base de datos con el metodo get
+app.get('/empleados', (req, res) => {
+    
+    db.query('SELECT * FROM empleados',
+    (err,result)=>{
+        if(err){
+            console.log(err)
+        }else{
+            res.send(result)
+        }
+    }) //Promesa de esos valores
+});
+
+//  INFO Función de actualización de la base de datos con el metodo put
 
 app.listen(3001, () => console.log('Server running on port 3001'));
