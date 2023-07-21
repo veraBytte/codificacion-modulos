4a05032e4f68f96e43d1cfc59210be45919248a9
Bryan Vera <brayanandresveragar@gmail.com>
2023-07-20 19:42:40 -0500

    Funcionalidad cancelar añadida

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
 
-  const [nombre,setNombre] = useState("");
-  const [edad,setEdad] = useState(0);
-  const [pais,setPais] = useState("");
-  const [cargo,setCargo] = useState("");
-  const [antiguedad,setAntiguedad] = useState(0);
-  const [id,setId] = useState(0);
-  const [editar,setEditar] = useState(false);
+  const [nombre, setNombre] = useState("");
+  const [edad, setEdad] = useState(0);
+  const [pais, setPais] = useState("");
+  const [cargo, setCargo] = useState("");
+  const [antiguedad, setAntiguedad] = useState(0);
+  const [id, setId] = useState(0);
+  const [editar, setEditar] = useState(false);
 
   const [empleadosList, setEmpleados] = useState([]);
 
@@ -25,6 +25,7 @@ function App() {
     }).then(() => {
       console.log("Success");
       getEmpleados();
+      limpiarCampos();
     });
   }
 
@@ -38,6 +39,7 @@ function App() {
       antiguedad: antiguedad
     }).then(() => {
       getEmpleados();
+      limpiarCampos();
     });
   }
 
@@ -49,6 +51,17 @@ function App() {
     setPais(empleado.PAIS);
     setAntiguedad(empleado.ANTIGUEDAD);
     setId(empleado.ID);
+  }
+
+  function limpiarCampos() {
+    setNombre("");
+    setEdad(0);
+    setPais("");
+    setCargo("");
+    setAntiguedad(0);
+    setId(0);
+
+    setEditar(false);
   }
 
   const getEmpleados = () => {
@@ -71,58 +84,58 @@ function App() {
           <div className="input-group mb-3">
             <span className="input-group-text" id="basic-addon1">Nombre</span>
             <input type="text" value={nombre}
-              onChange = {(event) => {
+              onChange={(event) => {
                 setNombre(event.target.value);
-              }}            
-            className="form-control" placeholder="Nombre" aria-label="Username" aria-describedby="basic-addon1"/>
+              }}
+              className="form-control" placeholder="Nombre" aria-label="Username" aria-describedby="basic-addon1" />
           </div>
 
           <div className="input-group mb-3">
             <span className="input-group-text" id="basic-addon1">Edad</span>
             <input type="number" value={edad}
-              onChange = {(event) => {
+              onChange={(event) => {
                 setEdad(event.target.value);
-              }}            
-            className="form-control" placeholder="Edad" aria-label="Username" aria-describedby="basic-addon1"/>
+              }}
+              className="form-control" placeholder="Edad" aria-label="Username" aria-describedby="basic-addon1" />
           </div>
 
           <div className="input-group mb-3">
             <span className="input-group-text" id="basic-addon1">Pais</span>
             <input type="text" value={pais}
-              onChange = {(event) => {
+              onChange={(event) => {
                 setPais(event.target.value);
-              }}        
-            className="form-control" placeholder="Pais" aria-label="Username" aria-describedby="basic-addon1"/>
+              }}
+              className="form-control" placeholder="Pais" aria-label="Username" aria-describedby="basic-addon1" />
           </div>
 
           <div className="input-group mb-3">
             <span className="input-group-text" id="basic-addon1">Cargo</span>
             <input type="text" value={cargo}
-              onChange = {(event) => {
+              onChange={(event) => {
                 setCargo(event.target.value);
-              }}        
-            className="form-control" placeholder="Cargo ocupacional" aria-label="Username" aria-describedby="basic-addon1"/>
+              }}
+              className="form-control" placeholder="Cargo ocupacional" aria-label="Username" aria-describedby="basic-addon1" />
           </div>
 
           <div className="input-group mb-3">
             <span className="input-group-text" id="basic-addon1">Antiguedad</span>
             <input type="number" value={antiguedad}
-              onChange = {(event) => {
+              onChange={(event) => {
                 setAntiguedad(event.target.value);
-              }}        
-            className="form-control" placeholder="Antiguedad" aria-label="Username" aria-describedby="basic-addon1"/>
+              }}
+              className="form-control" placeholder="Antiguedad" aria-label="Username" aria-describedby="basic-addon1" />
           </div>
 
         </div>
 
         <div className="card-footer text-body-secondary">
           {
-            editar?
-            <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-              <button className="btn btn-warning m-2" onClick={update}>Actualizar</button>    
-              <button type="button" className="btn btn-danger m-2">Cancelar</button>
-            </div>
-            :<button className="btn btn-success" onClick={add}>Registrar</button>
+            editar ?
+              <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                <button className="btn btn-warning m-2" onClick={update}>Actualizar</button>
+                <button type="button" className="btn btn-danger m-2" onClick={limpiarCampos}>Cancelar</button>
+              </div>
+              : <button className="btn btn-success" onClick={add}>Registrar</button>
           }
         </div>
       </div>
@@ -146,36 +159,36 @@ function App() {
           {
             empleadosList.map((empleado) => {
               return <tr key={empleado.ID}>
-                      <th scope="row">{empleado.ID}</th>
-                      <td>{empleado.NAME}</td>
-                      <td>{empleado.EDAD}</td>
-                      <td>{empleado.PAIS}</td>
-                      <td>{empleado.CARGO}</td>
-                      <td>{empleado.ANTIGUEDAD}</td>
-                      <td>
-                        <div className="btn-group" role="group" aria-label="Basic mixed styles example">
-                          <button type="button"
-                          onClick={() => {
-                            editarEmpleado(empleado);
-                          }}
-                          className="btn btn-warning">Editar</button>
+                <th scope="row">{empleado.ID}</th>
+                <td>{empleado.NAME}</td>
+                <td>{empleado.EDAD}</td>
+                <td>{empleado.PAIS}</td>
+                <td>{empleado.CARGO}</td>
+                <td>{empleado.ANTIGUEDAD}</td>
+                <td>
+                  <div className="btn-group" role="group" aria-label="Basic mixed styles example">
+                    <button type="button"
+                      onClick={() => {
+                        editarEmpleado(empleado);
+                      }}
+                      className="btn btn-warning">Editar</button>
 
-                          <button type="button" className="btn btn-danger">Eliminar</button>
-                        </div> 
-                      </td>
-                    </tr>
-              
-              
+                    <button type="button" className="btn btn-danger">Eliminar</button>
+                  </div>
+                </td>
+              </tr>
+
+
               // <div key={empleado.ID} className=""> </div>
             })
           }
-          
+
         </tbody>
       </table>
 
       <button className="btn btn-success" onClick={getEmpleados}>Consultar</button>
-      
-      
+
+
     </div>
   );
 }
